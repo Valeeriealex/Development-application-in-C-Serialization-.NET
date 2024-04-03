0620bdd8e62825c5fe0e78fe4ba0fdd7a0a04f23
Valerie <114693445+Valeeriealex@users.noreply.github.com>
2024-04-03 18:56:10 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Xml.Linq;
+
+namespace Convert_JSON_in_XML
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Данные в формате JSON
+            string jsonData = "{ \"name\": \"John\", \"age\": 30, \"city\": \"New York\" }";
+
+            //Использование Text.Json
+            JsonDocument jsonDocument = JsonDocument.Parse(jsonData);
+
+            //Создание документа и добавление элементов
+            XDocument xDocument = new XDocument(
+            new XElement("Root",
+                jsonDocument.RootElement.EnumerateObject()
+                    .Select(prop => new XElement(prop.Name, prop.Value.ToString()))
+            )
+        );
+            //Конвертация в XML
+            string xmlOutput = xDocument.ToString();
+            Console.WriteLine(xmlOutput);
+        }
+    }
+}
